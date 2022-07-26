366a1e076373a14e557c571210ea4d87a7c64565
Kaisurina <pokemonqqqwww@gmail.com>
2022-07-26 22:59:34 +0300

    adjustments

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,32 +1,33 @@
 import { observer } from "mobx-react-lite";
 import todolist from "../store/todolist";
 import bin from "../img/bin.svg";
-import { TodoProps } from "../interfaces";
+import { TodoProps, Todos } from "../interfaces";
 
 function TodoList({ listTodo }: TodoProps) {
   return (
     <div className="todoList">
-      {listTodo?.map((todo?: any) => (
-        <div
-          className={todo.completed ? "todoItem active" : "todoItem"}
-          key={todo.id}
-        >
-          <label className="todoCheckbox">
+      {listTodo &&
+        listTodo.map((todo: Todos) => (
+          <div
+            className={todo.completed ? "todoItem active" : "todoItem"}
+            key={todo.id}
+          >
+            <label className="todoCheckbox">
+              <input
+                onChange={() => todolist.statusTodo(todo.id)}
+                checked={todo.completed}
+                type="checkbox"
+              ></input>
+              <div className="checkbox__checkmark"></div>
+              <div className="checkbox__body">{todo.title}</div>
+            </label>
             <input
-              onChange={() => todolist.statusTodo(todo.id)}
-              checked={todo.completed}
-              type="checkbox"
+              onClick={() => todolist.removeTodo(todo.id)}
+              type="image"
+              src={bin}
             ></input>
-            <div className="checkbox__checkmark"></div>
-            <div className="checkbox__body">{todo.title}</div>
-          </label>
-          <input
-            onClick={() => todolist.removeTodo(todo.id)}
-            type="image"
-            src={bin}
-          ></input>
-        </div>
-      ))}
+          </div>
+        ))}
     </div>
   );
 }
